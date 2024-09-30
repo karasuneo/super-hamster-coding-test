c78f8e4eb7e8be1ad6dd65e13995a7b96a48e6fe
karasuneo <yusei4520030401@gmail.com>
2024-09-30 21:29:59 +0900

    add: ボタンコンポーネントに当てる可変マップの定義

---
+++ src/components/Button/const/index.ts
@@ -0,0 +1,27 @@
+export const BUTTON_SIZE_MAP: {
+  [key: string]: { width: string; height: string };
+} = {
+  xs: { width: '5rem', height: '2rem' },
+  sm: { width: '6rem', height: '2.5rem' },
+  md: { width: '8rem', height: '3rem' },
+  lg: { width: '10rem', height: '4rem' },
+  xl: { width: '12rem', height: '5rem' },
+};
+
+export const BUTTON_FONT_SIZE_MAP: { [key: string]: { fontSize: string } } = {
+  xs: { fontSize: '0.8rem' },
+  sm: { fontSize: '1rem' },
+  md: { fontSize: '1.2rem' },
+  lg: { fontSize: '1.4rem' },
+  xl: { fontSize: '1.6rem' },
+};
+
+export const LOADING_SIZE_MAP: {
+  [key: string]: { width: string; height: string };
+} = {
+  xs: { width: '8px', height: '8px' },
+  sm: { width: '12px', height: '12px' },
+  md: { width: '16px', height: '16px' },
+  lg: { width: '20px', height: '20px' },
+  xl: { width: '24px', height: '24px' },
+};
