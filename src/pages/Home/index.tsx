652e8bef38e3b8d0e8a317ead08c7f7d452655c4
karasuneo <yusei4520030401@gmail.com>
2024-09-30 21:38:21 +0900

    add: ボタンの状態・種類を並べたページを追加

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import EditIcon from '@/components/Icon/EditIcon';
+import styles from './styles.module.scss';
+
+import ButtonList from '@/features/DisplayButton/components/ButtonList';
+function Home() {
+  return (
+    <>
+      <div className={styles.buttonListContainer}>
+        <section className={styles.buttonListSection}>
+          <ButtonList isDark text="編集" icon={<EditIcon />} />
+          <ButtonList text="編集" icon={<EditIcon />} />
+          <ButtonList outline text="編集" icon={<EditIcon />} />
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default Home;
