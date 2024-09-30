6d147a1d11a6e566b66792106e3916084eec588d
karasuneo <yusei4520030401@gmail.com>
2024-10-01 08:34:45 +0900

    add: ChangeButtonIconPositionCheckBoxListを呼び出すようCSSおよびコンポーネントを追加

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,16 +3,18 @@ import styles from './styles.module.scss';
 import ButtonList from '@/features/DisplayButton/components/ButtonList';
 import ChangeButtonTypeCheckBoxList from '@/features/ChangeButtonType/components/ChangeButtonTypeCheckBoxList';
 import ChangeButtonSizeCheckBoxList from '@/features/ChangeButtonSize/components/CheckBoxList';
+import ChangeButtonIconPositionCheckBoxList from '@/features/ChangeButtonIconPosition/components/ChangeDisplayButtonIconCheckBoxList';
 
 function Home() {
   return (
     <>
       <div className={styles.container}>
-        <section className={styles.darkButtonSection}>
+        <section className={styles.buttonSection}>
           <div className={styles.buttonListContainer}>
             <ButtonList text="編集" icon={<EditIcon />} />
           </div>
           <div className={styles.checkBoxListContainer}>
+            <ChangeButtonIconPositionCheckBoxList />
             <ChangeButtonSizeCheckBoxList />
             <ChangeButtonTypeCheckBoxList />
           </div>
