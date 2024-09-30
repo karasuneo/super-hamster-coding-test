af2c1935d07bcf00cf620aa81194d5c6a9fe899b
karasuneo <yusei4520030401@gmail.com>
2024-10-01 01:15:27 +0900

    add: チェックボックスコンポーネントのサイズに関連する定数を定義

---
+++ src/components/CheckBox/const/index.ts
@@ -0,0 +1,17 @@
+export const CHECK_BOX_SIZE_MAP: { [key: string]: { fontSize: string } } = {
+  xs: { fontSize: '0.8rem' },
+  sm: { fontSize: '1rem' },
+  md: { fontSize: '1.2rem' },
+  lg: { fontSize: '1.4rem' },
+  xl: { fontSize: '1.6rem' },
+};
+
+export const CHECK_MARK_SIZE_MAP: {
+  [key: string]: { width: string; height: string };
+} = {
+  xs: { width: '8px', height: '8px' },
+  sm: { width: '12px', height: '12px' },
+  md: { width: '16px', height: '16px' },
+  lg: { width: '20px', height: '20px' },
+  xl: { width: '24px', height: '24px' },
+};
