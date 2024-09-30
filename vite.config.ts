e932f0231eed5bf234a6794d2336b045205a08cb
karasuneo <yusei4520030401@gmail.com>
2024-09-30 19:22:24 +0900

    chore: エイリアスの設定

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
         additionalData: `
           @use '@/styles/colors.scss' as *;
           @use '@/styles/variables.scss' as *;
-          @use '@/styles/mixins.scss' as *;
         `,
       },
     },
