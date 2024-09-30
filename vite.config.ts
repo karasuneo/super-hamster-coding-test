15f0ce65eb98bb0d56dc29a2975b30f49d474844
karasuneo <yusei4520030401@gmail.com>
2024-09-30 19:20:57 +0900

    chore: エイリアスの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `
+          @use '@/styles/colors.scss' as *;
+          @use '@/styles/variables.scss' as *;
+          @use '@/styles/mixins.scss' as *;
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+});
